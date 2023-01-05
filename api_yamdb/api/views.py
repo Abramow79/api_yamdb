428907242b713f9f13f75e8d6f06bfade45d8197
Желтяков Марк <zheltiakov.mark@gmail.com>
2023-01-05 20:31:04 +0300

    Добавлены api/views (ReviewViewSet/CommentViewSet)

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.decorators import permission_classes
+from reviews.models import Review, Title
+
+# permissions не писал, взял у Димы
+from .permissions import IsAdminmMderator, IsAuthorOrReadOnly
+from .serializers import CommentSerializer, ReviewSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """Вьюсет для обьектов модели Review."""
+
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthorOrReadOnly, IsAdminmMderator,)
+
+    def get_title(self):
+        """Возвращает объект текущего произведения."""
+        title_id = self.kwargs.get('title_id')
+        return get_object_or_404(Title, pk=title_id)
+
+    def get_queryset(self):
+        """Возвращает queryset c отзывами для текущего произведения."""
+        return self.get_title().reviews.all()
+
+    def perform_create(self, serializer):
+        """Создает отзыв для текущего произведения,
+        где автором является текущий пользователь."""
+        serializer.save(
+            author=self.request.user,
+            title=self.get_title()
+        )
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """Вьюсет для обьектов модели Comment."""
+
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorOrReadOnly, IsAdminmMderator,)
+
+    def get_review(self):
+        """Возвращает объект текущего отзыва."""
+        review_id = self.kwargs.get('review_id')
+        return get_object_or_404(Review, pk=review_id)
+
+    def get_queryset(self):
+        """Возвращает queryset c комментариями для текущего отзыва."""
+        return self.get_review().comments.all()
+
+    def perform_create(self, serializer):
+        """Создает комментарий для текущего отзыва,
+        где автором является текущий пользователь."""
+        serializer.save(
+            author=self.request.user,
+            review=self.get_review()
+        )
