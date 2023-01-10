5d96c25bbda11f6b6979a09e4f9be6d5f1208fac
Андрей Абрамов <89194795009@mail.ru>
2023-01-11 01:23:49 +0500

    Подготавливаю Develop перед смерживанием с final_version

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.conf import settings
 from django.db import models
@@ -139,28 +138,3 @@ class Comments(AbstractModelReviewComments):
         verbose_name = "Комментарий"
         verbose_name_plural = "Комментарии"
         default_related_name = "comments"
-=======
-from django.db import models
-
-
-class Genres(models.Model):
-    name = models.CharField(max_length=256,
-                            verbose_name='Название')
-    slug = models.SlugField(unique=True)
-
-
-class Categories(models.Model):
-    name = models.CharField(max_length=256,
-                            verbose_name='Название')
-    slug = models.SlugField(unique=True)
-
-
-class Title(models.Model):
-    name = models.CharField(max_length=256,
-                            verbose_name='Название')
-    year = models.IntegerField(verbose_name='Год выпуска')
-    description = models.TextField(verbose_name='Описание')
-    genre = models.ForeignKey(Genres, on_delete=models.SET_NULL, null=True,)
-    category = models.ForeignKey(Categories, on_delete=models.SET_NULL,
-                                 null=True)
->>>>>>> dev2_Models_Views_URLs
